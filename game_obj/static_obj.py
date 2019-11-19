1dccd9046fd2078ebf60431718c32d57df3579d3
SeungminLee-SASA4 <43242333+SeungminLee-SASA4@users.noreply.github.com>
2019-11-19 11:37:02 +0900

    움직이지안흔객체

--- game_obj/static_obj.py
+++ game_obj/static_obj.py
@@ -27,17 +27,18 @@ class static(): #움직이지 않는 객체의 클래스
 
 
 class cupbab(static): #컵밥 클래스
-    
+
     def eaten(bab, field): # 컵밥의 정보가 든 리스트 bab, 객체 전부의 정보가 든 리스트 field를 받아서 객체를 삭제.
         for i in bab:
             if self.r_pos == i.r_pos and self.c_pos == i.c_pos:
                 bab.remove(i)
-        for i in field:
-            if self.r_pos == i.r_pos and self.y_pos == i.c_pos:
-                field.remove(i)
+                
+        field[self.r_pos][self.c_pos] = 0
 
 
 class trap(static): #함정 클래스
+
+    
     pass #함정이 생성되고 나서는 아무것도 안 함.
 
 
@@ -48,13 +49,24 @@ for i in range(15):
     for j in range(15):
         line.append(0)
     field.append(line)
+
+cubbab = []
     
 cnt = 0
 
 while True:
     a = cupbab(field)
+    cubbab.append(a)
     print(a.r_pos, a.c_pos, cnt, a)
     cnt += 1
+    if cnt>=50:
+        break
+
+for i in cubbab:
+    print(i.r_pos, i.c_pos)
+    
+
+
 
 
 
