a708ee150545e20c9c6aa141f412900c0eda935f
acetop210 <acetop210@sasa.hs.kr>
2019-11-19 11:32:38 +0900

    random_move fixed

--- game_obj/dynamic.py
+++ game_obj/dynamic.py
@@ -20,18 +20,28 @@ class dynamic():
             else:
                 self.y_pos = self.y_pos - 1
 
-    def random_move(self):
+    def random_move(self, world):
         point = [1, 0, -1]
         pointx = [1, -1]
-        x_direction = random.choice(point)
+        new_x = self.x_pos
+        new_y = self.y_pos
 
-        if x_direction == 0:
-            y_direction = random.choice(pointx)
-        else:
-            y_direction = 0
+        while world[new_x][new_y] != '0':
+            new_x = self.x_pos
+            new_y = self.y_pos
 
-        self.x_pos = x_direction
-        self.y_pos = y_direction
+            x_dir = random.choice(point)
+
+            if x_dir == 0:
+                y_dir = random.choice(pointx)
+            else:
+                y_dir = 0
+
+            new_x += x_dir
+            new_y += y_dir
+
+        self.x_pos = new_x
+        self.y_pos = new_y
 
     def minus_health(self):
         self.health = self.health - 1
