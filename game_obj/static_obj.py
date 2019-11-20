bbbb35cdf36307c8f7779924ca4068c54cea5fd5
SeungminLee-SASA4 <43242333+SeungminLee-SASA4@users.noreply.github.com>
2019-11-20 12:31:02 +0900

    Add files via upload

--- game_obj/static_obj.py
+++ game_obj/static_obj.py
@@ -1,9 +1,23 @@
+
 import random
+import pygame
+
+BLACK= ( 0,  0,  0)
+WHITE= (255,255,255)
+BLUE = ( 0,  0,255)
+GREEN= ( 0,255,  0)
+RED  = (255,  0,  0)
+
+pygame.display.set_caption("DABANG JABGI")
+size  = [400,300]
+screen= pygame.display.set_mode(size)
+
+cupbab_image = pygame.image.load("cupbab.jpg")
 
 
-class static():  # 움직이지 않는 객체의 클래스
+class static(): #움직이지 않는 객체의 클래스
 
-    def __init__(self, field):  # 판의 정보 field를 받아서 비어있는 곳에 객체를 생성.
+    def __init__(self, field): # 판의 정보 field를 받아서 비어있는 곳에 객체를 생성.
         self.r_pos = 0
         self.c_pos = 0
 
@@ -11,12 +25,12 @@ class static():  # 움직이지 않는 객체의 클래스
         if len(field) == 0:
             self.r_pos = random.randint(1, 10)
             self.c_pos = random.randint(1, 10)
-            flag = 1
+            flag=1
         else:
             while True:
                 flag = 0
-                r_rand = random.randint(1, 10)
-                c_rand = random.randint(1, 10)
+                r_rand = random.randint(1,10)
+                c_rand = random.randint(1,10)
                 if field[r_rand][c_rand] == 0:
                     self.r_pos = r_rand
                     self.c_pos = c_rand
@@ -25,23 +39,34 @@ class static():  # 움직이지 않는 객체의 클래스
         if flag:
             field[self.r_pos][self.c_pos] = 1
 
-
-class cupbab(static):  # 컵밥 클래스
-
-    def eaten(self, bab, field):  # 컵밥의 정보가 든 리스트 bab, 판의 정보 field를 받아서 객체를 삭제.
-        for i in bab:
-            if self.r_pos == i.r_pos and self.c_pos == i.c_pos:
-                bab.remove(i)
-
-        field[self.r_pos][self.c_pos] = 0
+    def draw(self, screen):
+        position = (self.c_pos*10, self.r_pos*10, 30, 30)
+        screen.blit(self.kind, position)
+    
 
 
-class trap(static):  # 함정 클래스
+class cupbab(static): #컵밥 클래스
 
     def __init__(self, field):
         static.__init__(self, field)
         self.health = 3
+        self.kind = GREEN
 
+    def eaten(self, bab, field): # 컵밥의 정보가 든 리스트 bab, 판의 정보 field를 받아서 객체를 삭제.
+        for i in bab:
+            if self.r_pos == i.r_pos and self.c_pos == i.c_pos:
+                bab.remove(i)
+                
+        field[self.r_pos][self.c_pos] = 0
+
+
+class trap(static): #함정 클래스
+
+    def __init__(self, field):
+        static.__init__(self, field)
+        self.health = 3
+        self.kind = cupbab_image
+    
     def delete(self, Trap):
         for i in Trap:
             if self.r_pos == i.r_pos and self.c_pos == i.c_pos:
@@ -55,21 +80,47 @@ class trap(static):  # 함정 클래스
             return 1
         else:
             return 0
+        
+        
+
+        
 
 
 field = []
 for i in range(15):
     line = []
     for j in range(15):
+        
         line.append(0)
     field.append(line)
 
 Trap = []
-
+    
 cnt = 0
 
 a = trap(field)
 
-print(a.health)
-a.fellin()
-print(a.istrap())
+a.draw(screen)
+
+
+
+
+pygame.display.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+        
