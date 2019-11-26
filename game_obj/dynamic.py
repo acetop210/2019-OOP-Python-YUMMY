fcb3163a848026126eb12b6d2779f59c61b79a87
yeomjoochan <yjch1717@sasa.hs.kr>
2019-11-26 11:10:13 +0900

    다이나믹에 모든 캐릭터 넣음

--- game_obj/dynamic.py
+++ game_obj/dynamic.py
@@ -128,6 +128,46 @@ class FourGi(Dynamic):
         if die is True:
             four_list.remove(self)
 
+
+class FourGi(Dynamic):
+    def __init__(self):
+        Dynamic.__init__(self)
+        self.health = 3
+
+    def eat5(self, five_list):
+        for i in five_list:
+            if i.x_pos == self.x_pos and i.y_pos == self.y_pos:
+                five_list.remove(i)
+                break
+
+    def move4(self, world, four_list, five_list, trap_list):
+        dx = [1, 0, -1, 0]
+        dy = [0, 1, 0, -1]
+        find5 = False
+        for i in range(4):
+            nx = self.x_pos + dx
+            ny = self.y_pos + dy
+            if nx <= 0 or nx > 10 or ny <= 0 or ny > 10:
+                continue
+            if world[nx][ny] == '5':
+                self.x_pos = nx
+                self.y_pos = ny
+                self.eat5(five_list)
+                find5 = True
+                break
+        if find5 is False:
+            self.random_move(world)
+            self.fallen(four_list, trap_list)
+
+    def fallen(self, four_list, trap_list):
+        die = False
+        for i in trap_list:
+            if self.x_pos == i.x_pos and self.y_pos == i.y_pos:
+                die = True
+                break
+        if die is True:
+            four_list.remove(self)
+
 class FiveGi(Dynamic):
     def __init__(self):
         Dynamic.__init__(self)
